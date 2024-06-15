5c32ea55060eda7b663b3bf07fd2def7614dd201
shraddha5chopra <75442009+shraddha5chopra@users.noreply.github.com>
2024-06-15 20:41:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import app from "./app.js";
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
 
